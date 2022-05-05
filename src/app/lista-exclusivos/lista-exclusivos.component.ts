ff9845fa7b49092e3b03fe8565ae1d977f204379
EstebanSanchez3 <esteban2324.sanchez@gmail.com>
2022-05-05 14:12:00 +0200

    Sólo queda rematar cajas, responsive y buscar
    Sólo queda rematar cajas, responsive y buscar

--- src/app/lista-exclusivos/lista-exclusivos.component.ts
+++ src/app/lista-exclusivos/lista-exclusivos.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 //EXPORT DE CLASS MODELS  
 //DECLARACIÓN PARA EXPORTAR DE SERVICE
-  import { exclusivo } from '../models/exclusivo.model';
-  import { SExclusivoService } from '../services/s-exclusivo.service';
+import { exclusivo } from '../models/exclusivo.model';
+import { SExclusivoService } from '../services/s-exclusivo.service';
 
 @Component({
   selector: 'app-lista-exclusivos',
@@ -12,21 +12,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaExclusivosComponent implements OnInit {
 
-//ARRAY ALMACENA TODO  
-//(1ºNOMBRE BBDD)(2ºNOMRE DE EVENTO)
-//DIFERENCIA COLORES COMO EN SERVICE
-//↓ESTE(1ºexclusivos_ps4)EN "OF" DENTRO DE "NGFOR" HTML del mismo "CAJA"
-  exclusivos_ps4:exclusivo[] = new Array<exclusivo>() ;
+  //ARRAY ALMACENA TODO  
+  //(1ºNOMBRE BBDD)(2ºNOMRE DE EVENTO)
+  //DIFERENCIA COLORES COMO EN SERVICE
+  //↓ESTE(1ºexclusivos_ps4)EN "OF" DENTRO DE "NGFOR" HTML del mismo "CAJA"
+  exclusivos_ps4: exclusivo[] = new Array<exclusivo>();
 
-//DESPUÉS APARECE EN NGONINI
-  constructor(private servicioexclusivo:SExclusivoService) {
-   }
+  //DESPUÉS APARECE EN NGONINI
+  constructor(private servicioexclusivo: SExclusivoService) {
+  }
 
-//CASI TODO SE GENERA AUTOMÁTICO POR QUE YA LO HAS METIDO ANTES
+
+  //CASI TODO SE GENERA AUTOMÁTICO POR QUE YA LO HAS METIDO ANTES
+  //"DATOS" ES EL NOMBRE DONDE SE GUARDA PETICIÓN
   ngOnInit(): void {
-    this.servicioexclusivo.getexclusivo().subscribe(
-         exclusivos_ps4 =>{
-         this.exclusivos_ps4=this.exclusivos_ps4 ;
-       })
+    this.servicioexclusivo.getexclusivo().subscribe(datos => {
+      this.exclusivos_ps4 = datos;
+    });
   }
 }
--- src/app/services/s-exclusivo.service.ts
+++ src/app/services/s-exclusivo.service.ts
@@ -2,32 +2,31 @@ import { Injectable } from "@angular/core";
 import { Observable, observable } from "rxjs";
 
 //IMPORT DEL MODELS CLASS EXCLUSIVO
-  import { exclusivo } from "../models/exclusivo.model";
+import { exclusivo } from "../models/exclusivo.model";
 //SE IMPORTA EN APP.MODULE Y SE USA AQUÍ
-  import { HttpClient } from '@angular/common/http'  
+import { HttpClient } from '@angular/common/http'
 //
-  @Injectable({
-    providedIn: 'root'
+@Injectable({
+  providedIn: 'root'
 })
 //DECLARACIÓN PARA EXPORTAR SERVICE
-  export class SExclusivoService {
+export class SExclusivoService {
 
-//(1ºNOMBRE BBDD)(2ºNOMRE DE EVENTO)
-//SE DIFERENCIAN POR LOS COLORES
+  //(1ºNOMBRE BBDD)(2ºNOMRE DE EVENTO)
+  //SE DIFERENCIAN POR LOS COLORES
   exclusivos_ps4: Observable<exclusivo[]> = new Observable();
 
-//DESPUÉS APARECE EN CONEXIÓN CON ECLIPSE ABAJO 
+  //DESPUÉS APARECE EN CONEXIÓN CON ECLIPSE ABAJO 
   constructor(private clientHttp: HttpClient) {
   }
 
-  getexclusivo(): Observable<exclusivo[]>{
-      console.log("Obteniendo exclusivos del ws");
+  getexclusivo(): Observable<exclusivo[]> {
+    console.log("Obteniendo exclusivos del ws");
 
-//CONEXIÓN CON ECLIPSE
-  this.exclusivos_ps4 = this.clientHttp.get<exclusivo[]>
-  
-//CONECTA CON @GetMapping DE RESTCONTOLLER 
-  ("http://localhost:8080/exclusivos")
-  return this.exclusivos_ps4
-    }
+    //CONEXIÓN CON ECLIPSE
+    //CONECTA CON @GetMapping DE RESTCONTOLLER 
+    this.exclusivos_ps4 = this.clientHttp.get<exclusivo[]>("http://localhost:8080/exclusivos")
+    return this.exclusivos_ps4
+      
+  }
 }
